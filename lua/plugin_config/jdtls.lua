721c8c2941d94d5ac9ce7bacc2cae75b23f611c2
cccccmake <kechou.dai@gmail.com>
2023-05-30 11:57:40 +0200

    discarded nvim-jdtls

--- lua/plugin_config/jdtls.lua
+++ lua/plugin_config/jdtls.lua
@@ -26,13 +26,10 @@ local config = {
     cmd = {
         'jdtls',
     },
-    -- The command that starts the language server
-    -- See: https://github.com/eclipse/eclipse.jdt.ls#running-from-the-command-line
     -- cmd = {
     --
     --     -- 'java', -- or '/path/to/java17_or_newer/bin/java'
     --     '/usr/lib/jvm/java-20-openjdk/bin/java',
-    --     -- depends on if `java` is in your $PATH env variable and if it points to the right version.
     --     '-Declipse.application=org.eclipse.jdt.ls.core.id1',
     --     '-Dosgi.bundles.defaultStartLevel=4',
     --     '-Declipse.product=org.eclipse.jdt.ls.core.product',
@@ -41,26 +38,15 @@ local config = {
     --     'java.base/java.util=ALL-UNNAMED', '--add-opens',
     --     'java.base/java.lang=ALL-UNNAMED', '-jar',
     --     '/home/cccccmake/.local/share/lsp/plugins/org.eclipse.equinox.launcher_1.6.400.v20210924-0641.jar',
-    --     -- ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^                                       ^^^^^^^^^^^^^^
-    --     -- Must point to the                                                     Change this to
-    --     -- eclipse.jdt.ls installation                                           the actual version
     --     '-configuration', '/home/cccccmake/.local/share/lsp/config_linux/',
-    --     -- ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^        ^^^^^^
-    --     -- Must point to the                      Change to one of `linux`, `win` or `mac`
-    --     -- eclipse.jdt.ls installation            Depending on your system.
-    --     -- 💀
-    --     -- See `data directory configuration` section in the README
     --     '-data', workspace_dir
     -- },
 
-    -- 💀
     -- This is the default if not provided, you can remove it. Or adjust as needed.
     -- One dedicated LSP server & client will be started per unique root_dir
     root_dir = require('jdtls.setup').find_root({'.git', 'mvnw', 'gradlew'}),
 
-    -- Here you can configure eclipse.jdt.ls specific settings
     -- See https://github.com/eclipse/eclipse.jdt.ls/wiki/Running-the-JAVA-LS-server-from-the-command-line#initialize-request
-    -- for a list of options
     settings = {
         -- java = {
         --     eclipse = {downloadSources = true},
--- lua/plugin_config/nvim-lsp.lua
+++ lua/plugin_config/nvim-lsp.lua
@@ -86,7 +86,7 @@ end
 
 -- Use a loop to conveniently both setup defined servers 
 -- and map buffer local keybindings when the language server attaches
-local servers = { "pyright", "tsserver", "jdtls", "clangd"}
+local servers = { "pyright", "tsserver", "clangd", "lua_ls", "jdtls"}
 for _, lsp in ipairs(servers) do
   nvim_lsp[lsp].setup { on_attach = on_attach }
 end
